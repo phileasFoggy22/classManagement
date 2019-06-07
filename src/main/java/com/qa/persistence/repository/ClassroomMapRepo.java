438772f369f1d2ecdb71e63a326a4aa4f0aefae1
phileasFoggy22 <phileasFoggy@gmail.com>
2019-06-07 15:55:48 +0100

    Unable to Build Maven

--- src/main/java/com/qa/persistence/repository/ClassroomMapRepo.java
+++ src/main/java/com/qa/persistence/repository/ClassroomMapRepo.java
@@ -1,56 +1,61 @@
-package com.qa.persistence.repository;
-
-import java.util.Map;
-
-import com.qa.persistence.domain.Classroom;
-import com.qa.util.JSONUtil;
-
-public class ClassroomMapRepo implements ClassroomRepository {
-
-	private Map<Integer, Classroom> classes;
-
-	@Override
-	public String getClassrooms() {
-		return new JSONUtil().getJSONForObject(this.classes);
-	}
-
-	public ClassroomMapRepo(Map<Integer, Classroom> classAttendees) {
-		super();
-		this.classes = classAttendees;
-
-	}
-
-	@Override
-	public String createClassroom(String classroomJSON) {
-
-		Classroom class1 = new JSONUtil().getObjectForJSON(classroomJSON, Classroom.class);
-
-		this.classes.put(class1.getClassroomID(), class1);
-		return "Class Added";
-	}
-
-	@Override
-	public int countClassrooms() {
-		return this.classes.size();
-	}
-
-	@Override
-	public String deleteClassroom(int classID) {
-		this.classes.remove(classID);
-		return "Class Removed";
-	}
-
-	@Override
-	public String updateClassroom(int classID, String classroomJSON) {
-		Classroom class1 = new JSONUtil().getObjectForJSON(classroomJSON, Classroom.class);
-		this.classes.put(classID, class1);
-		return "Class Updated";
-	}
-
-	@Override
-	public String getClassroom(int classID) {
-
-		return new JSONUtil().getJSONForObject(this.classes.get(classID));
-	}
-
-}
+// package com.qa.persistence.repository;
+//
+// import java.util.Map;
+//
+// import javax.enterprise.inject.Alternative;
+//
+// import com.qa.persistence.domain.Classroom;
+// import com.qa.util.JSONUtil;
+//
+// @Alternative
+// public class ClassroomMapRepo implements ClassroomRepository {
+//
+// private Map<Integer, Classroom> classes;
+//
+// @Override
+// public String getClassrooms() {
+// return new JSONUtil().getJSONForObject(this.classes);
+// }
+//
+// public ClassroomMapRepo(Map<Integer, Classroom> classAttendees) {
+// super();
+// this.classes = classAttendees;
+//
+// }
+//
+// @Override
+// public String createClassroom(String classroomJSON) {
+//
+// Classroom class1 = new JSONUtil().getObjectForJSON(classroomJSON,
+// Classroom.class);
+//
+// this.classes.put(class1.getClassroomID(), class1);
+// return "Class Added";
+// }
+//
+// @Override
+// public int countClassrooms() {
+// return this.classes.size();
+// }
+//
+// @Override
+// public String deleteClassroom(int classID) {
+// this.classes.remove(classID);
+// return "Class Removed";
+// }
+//
+// @Override
+// public String updateClassroom(int classID, String classroomJSON) {
+// Classroom class1 = new JSONUtil().getObjectForJSON(classroomJSON,
+// Classroom.class);
+// this.classes.put(classID, class1);
+// return "Class Updated";
+// }
+//
+// @Override
+// public String getClassroom(int classID) {
+//
+// return new JSONUtil().getJSONForObject(this.classes.get(classID));
+// }
+//
+// }
