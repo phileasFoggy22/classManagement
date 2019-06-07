81c1e7a6f1fed5944e018307eb00e2c27f6f5726
phileasFoggy22 <phileasFoggy@gmail.com>
2019-06-07 15:09:39 +0100

    Rest Layer Added

--- src/main/java/com/qa/rest/ClassroomEndpoint.java
+++ src/main/java/com/qa/rest/ClassroomEndpoint.java
@@ -1,9 +1,12 @@
 package com.qa.rest;
 
 import javax.inject.Inject;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import com.qa.business.service.ClassroomService;
@@ -20,10 +23,38 @@ public class ClassroomEndpoint {
 		return service.getClassrooms();
 	}
 
-	@Path("/Classroom")
+	@Path("/createClassroom")
 	@POST
 	@Produces({ "application/json" })
 	public String createClassroom(String classroomJSON) {
 		return service.createClassroom(classroomJSON);
+	}
+
+	@Path("/deleteClassroom/{classID}")
+	@DELETE
+	@Produces({ "application/json" })
+	public String deleteClassroom(@PathParam("classID") int classID) {
+		return service.deleteClassroom(classID);
+	}
+
+	@Path("/updateClassroom/{classID}")
+	@PUT
+	@Produces({ "application/json" })
+	public String updateAccount(@PathParam("classID") int classID, String classroomJSON) {
+		return service.updateClassroom(classID, classroomJSON);
+	}
+
+	@Path("/countClassrooms")
+	@GET
+	@Produces({ "application/json" })
+	public int countClassrooms() {
+		return service.countClassrooms();
+	}
+
+	@Path("/getClassrooms/{classID}")
+	@GET
+	@Produces({ "application/json" })
+	public String getClassroom(@PathParam("classID") int classID) {
+		return service.getClassroom(classID);
 	}
 }
