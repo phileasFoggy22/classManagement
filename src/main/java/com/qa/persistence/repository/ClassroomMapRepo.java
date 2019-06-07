ed3ff837a87481bca4149ae46a35da3a15e704ce
phileasFoggy22 <phileasFoggy@gmail.com>
2019-06-07 14:09:37 +0100

    80% Tested

--- src/main/java/com/qa/persistence/repository/ClassroomMapRepo.java
+++ src/main/java/com/qa/persistence/repository/ClassroomMapRepo.java
@@ -9,6 +9,7 @@ public class ClassroomMapRepo implements ClassroomRepository {
 
 	private Map<Integer, Classroom> classes;
 
+	@Override
 	public String getClassrooms() {
 		return new JSONUtil().getJSONForObject(this.classes);
 	}
@@ -19,20 +20,37 @@ public class ClassroomMapRepo implements ClassroomRepository {
 
 	}
 
-	public Map<Integer, Classroom> getClassAttendees() {
-		return classes;
-	}
-
-	public void setClassAttendees(Map<Integer, Classroom> classAttendees) {
-		this.classes = classAttendees;
-	}
-
+	@Override
 	public String createClassroom(String classroomJSON) {
 
 		Classroom class1 = new JSONUtil().getObjectForJSON(classroomJSON, Classroom.class);
 
-		this.getClassAttendees().put(class1.getClassroomID(), class1);
-		return classroomJSON;
+		this.classes.put(class1.getClassroomID(), class1);
+		return "Class Added";
+	}
+
+	@Override
+	public int countClassrooms() {
+		return this.classes.size();
+	}
+
+	@Override
+	public String deleteClassroom(int classID) {
+		this.classes.remove(classID);
+		return "Class Removed";
+	}
+
+	@Override
+	public String updateClassroom(int classID, String classroomJSON) {
+		Classroom class1 = new JSONUtil().getObjectForJSON(classroomJSON, Classroom.class);
+		this.classes.put(classID, class1);
+		return "Class Updated";
+	}
+
+	@Override
+	public String getClassroom(int classID) {
+
+		return new JSONUtil().getJSONForObject(this.classes.get(classID));
 	}
 
 }
